3ddd30ebf94f1ef7d94ac0d694a0c1d9031bd1c4
Johan Jordaan <johan@inmydiary.com>
2016-12-11 08:01:59 +1100

    Added histogram and split into k-mers

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,8 @@
 module Lib
     ( patternCount,
       increment,
---      histogram,
+      histogram,
+      splitIntoKMers,
       mostFrequentKMers
     ) where
 
@@ -10,20 +11,40 @@ import Data.String.Utils
 import qualified Data.ByteString.Lazy.Char8 as C
 import qualified Data.Map as M
 
-_patternCount :: [Char] -> [Char] -> Int -> Int
-_patternCount pattern [] a = a
-_patternCount pattern text a
-  | startswith pattern text = _patternCount pattern (tail text) (a+1)
-  | otherwise = _patternCount pattern (tail text) a
+patternCount' :: [Char] -> [Char] -> Int -> Int
+patternCount' pattern [] a = a
+patternCount' pattern text a
+  | startswith pattern text = patternCount' pattern (tail text) (a+1)
+  | otherwise = patternCount' pattern (tail text) a
 
 patternCount :: [Char] -> [Char] -> Int
-patternCount pattern text = _patternCount pattern text 0
+patternCount pattern text = patternCount' pattern text 0
 
 increment :: [Char] -> (M.Map [Char] Int) -> (M.Map [Char] Int)
 increment k m = M.insert k ((M.findWithDefault 0 k m ) +1) m
 
---histogram :: [[Char]] -> (M.Map [Char] Int)
---histogram l = increment
+histogram' :: [[Char]] -> (M.Map [Char] Int) -> (M.Map [Char] Int)
+histogram' [] m = m
+histogram' (x:xs) m = histogram' xs (increment x m)
+
+histogram :: [[Char]] -> (M.Map [Char] Int)
+histogram l = histogram' l M.empty
+
+
+drop' k text = C.unpack $ C.drop (fromIntegral k) $ C.pack text
+take' k text = C.unpack $ C.take (fromIntegral k) $ C.pack text
+
+splitIntoKMers' :: [Char] -> Int -> [[Char]] -> [[Char]]
+splitIntoKMers' [] k r = r
+splitIntoKMers' text k r
+   | length text < k = r
+   | otherwise = splitIntoKMers' (drop' 1 text) k (r ++ [(take' k text)])
+
+splitIntoKMers :: [Char] -> Int -> [[Char]]
+splitIntoKMers text k = splitIntoKMers' text k []
 
 mostFrequentKMers :: Int -> [Char] -> [[Char]]
-mostFrequentKMers k text = ["CATG","GCAT"]
+mostFrequentKMers text k = histogram $ splitIntoKMers text k
+
+
+--["CATG","GCAT"]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,7 @@
 import Test.Hspec
 import Control.Exception (evaluate)
 import qualified Data.Map as M
-import Lib (patternCount, increment, mostFrequentKMers)
+import Lib (patternCount, increment, histogram, splitIntoKMers, mostFrequentKMers)
 
 
 main :: IO ()
@@ -29,7 +29,33 @@ main = hspec $ do
          (length m4) `shouldBe` (1 :: Int)
          m4 M.! k `shouldBe` (3 :: Int)
 
+      it "should increment the correct keys" $ do
+         let m = M.fromList [ ("a",10) , ("b",20) ]
+         let m2 = increment "a" m
+         let m3 = increment "b" m2
+         let m4 = increment "c" m3
+         (length m4) `shouldBe` (3 :: Int)
+         m4 M.! "a" `shouldBe` (11 :: Int)
+         m4 M.! "b" `shouldBe` (21 :: Int)
+         m4 M.! "c" `shouldBe` (1 :: Int)
 
+
+   describe "histogram" $ do
+      it "should create a histogram from the input data" $ do
+         let l = ["a","b","c","a","a","c","c","c"]
+         let m = histogram l
+         (length m) `shouldBe` (3 :: Int)
+         m M.! "a" `shouldBe` (3 :: Int)
+         m M.! "b" `shouldBe` (1 :: Int)
+         m M.! "c" `shouldBe` (4 :: Int)
+
+   describe "splitIntoKMers" $ do
+      it "should split the text into n k-mers" $ do
+         let text = "ACGTTGCATGTCGCATGATGCATGAGAGCT"
+         let k = 4
+         let l = splitIntoKMers text 4
+         (length l) `shouldBe` (((length text) -k +1) :: Int)
+         l !! 0  `shouldBe` ("ACGT" :: [Char])
 
    describe "mostFrequentKMers" $ do
       it "returns the list of most frequenct k-mers in the text" $ do
